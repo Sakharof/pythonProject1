4ef4f8a9a513d5c6ee6bd7e6e4b66b45e4431fc7
Mikhail <133436775+Sakharof@users.noreply.github.com>
2023-10-09 15:29:40 +0300

    #Second test

--- Ferst test.py
+++ Ferst test.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import math
 
-link = "http://suninjuly.github.io/simple_form_find_task.html"
+link = " http://suninjuly.github.io/find_link_text"
 
 try:
     browser = webdriver.Chrome()
     browser.get(link)
+    link = browser.find_element(By.LINK_TEXT, "224592")
+    link.click()
 
     input1 = browser.find_element(By.TAG_NAME, "input")
     input1.send_keys("Ivan")
